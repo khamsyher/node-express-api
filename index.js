a917d8a6e309fc0af7957989565f838f1730d523
khamsyher <114057321+khamsyher@users.noreply.github.com>
2023-09-05 11:11:14 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = requried('express')
+const app = express()
+const PORT = 4000
+
+app.listen(PORT, ()=>{
+    console.log(`API Listening On Port ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('This is API Runing')
+});
+
+app.get('about', (req, res) =>{
+    res.send('about page ')
+})
+
+module.exports = app
+
